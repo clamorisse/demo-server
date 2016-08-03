fad2d131ff1e9f22946461aa099e02cd50219618
clamorisse <bvcotero@gmail.com>
2016-08-03 11:19:06 -0400

    Dir for terraform

---
+++ terraform/modules/nat/nat.tf
@@ -0,0 +1,21 @@
+#--------------------------------------------------------------
+#       MODULE THAT CREATES NAT GATEWAY
+#--------------------------------------------------------------
+
+variable "name"             { default = "nat" }
+variable "az"               { }
+variable "public_subnet_id" { }
+
+resource "aws_eip" "nat" {
+  vpc   = true
+  lifecycle { create_before_destroy = true }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${var.public_subnet_id}"
+
+  lifecycle { create_before_destroy = true }
+}
+
+output "nat_gateway_id" { value = "${aws_nat_gateway.nat.id}" }
